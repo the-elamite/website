9f96920b5338c902473d6edc4dcd383b3cde8f9c
alalngar <elamite_tepe@proton.me>
2022-12-05 16:01:13 -0600

    Update post.php

--- post.php
+++ post.php
@@ -7,9 +7,9 @@ $db = new SQLite3('threads.db');
 $stmt = $db->prepare("INSERT INTO threads(time, author, title, message)"
     . "VALUES(:time, :author, :title, :message)");
 $stmt->bindValue(':time', time(), SQLITE3_INTEGER);
-$stmt->bindValue(':author', filter_input(INPUT_POST, $_POST['author'], FILTER_SANITIZE_STRING));
-$stmt->bindValue(':title', filter_input(INPUT_POST, $_POST['title'], FILTER_SANITIZE_STRING));
-$stmt->bindValue(':message', filter_input(INPUT_POST, $_POST['message'], FILTER_SANITIZE_STRING));
+$stmt->bindValue(':author', strip_tags($_POST['author']));
+$stmt->bindValue(':title', strip_tags($_POST['title']));
+$stmt->bindValue(':message', strip_tags($_POST['message']));
 $stmt->execute();
 
 $res = $db->query("SELECT * FROM threads");
