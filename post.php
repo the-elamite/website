4c2a65239aec6c87b67b74496db3a9d279d0bfdf
alalngar <elamite_tepe@proton.me>
2022-12-05 15:59:39 -0600

    Update post.php

--- post.php
+++ post.php
@@ -7,14 +7,14 @@ $db = new SQLite3('threads.db');
 $stmt = $db->prepare("INSERT INTO threads(time, author, title, message)"
     . "VALUES(:time, :author, :title, :message)");
 $stmt->bindValue(':time', time(), SQLITE3_INTEGER);
-$stmt->bindValue(':author', $_POST['author']);
-$stmt->bindValue(':title', $_POST['title']);
-$stmt->bindValue(':message', $_POST['message']);
+$stmt->bindValue(':author', filter_input(INPUT_POST, $_POST['author'], FILTER_SANITIZE_STRING));
+$stmt->bindValue(':title', filter_input(INPUT_POST, $_POST['title'], FILTER_SANITIZE_STRING));
+$stmt->bindValue(':message', filter_input(INPUT_POST, $_POST['message'], FILTER_SANITIZE_STRING));
 $stmt->execute();
 
 $res = $db->query("SELECT * FROM threads");
 while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
-    echo '<pre>' . $row['author'] . ', ' . $row['title'] . ': ' . $row['message'] . '</pre><br/>';
+    echo $row['author'] . ', ' . $row['title'] . ': ' . $row['message'] . '<br/>';
 }
 ?>
 
