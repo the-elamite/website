d4e16c148ad2a68e2f0a18dae7d3e4d048186215
alalngar <elamite_tepe@proton.me>
2022-12-05 14:33:48 -0600

    Update post.php

--- post.php
+++ post.php
@@ -5,7 +5,7 @@
 <?php
 $db = new SQLite3('threads.db');
 $statement = $db->prepare('INSERT INTO threads(id, time, author, title, message) VALUES (null, :time, :author, :title, :message)');
-$statement->bindValue(':time', time());
+$statement->bindValue(':time', time(), SQLITE3_INTEGER);
 $statement->bindValue(':author', $_POST['author']);
 $statement->bindValue(':title', $_POST['title']);
 $statement->bindValue(':message', $_POST['message']);
