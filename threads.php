81d003ecd8b12478d1158342675cb15af83efc23
alalngar <elamite_tepe@proton.me>
2022-12-05 17:02:03 -0600

    Create threads.php

---
+++ threads.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to swagtopia!</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="title">
+		<h1>swagtopia</h1>
+		<p>the only correct opinion is my own.</p>
+	</div>
+
+	<div class="main">
+        <?php
+        $db = new SQLite3('threads.db');
+        $stmt = $db->prepare("SELECT time, author, title, message FROM threads WHERE id=:id");
+        $stmt->bindValue(":id", $_GET["id"]);
+        $res = $stmt->execute();
+
+        while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+            echo $row['message'];
+        }
+        ?>
+	</div>
+
+	<div class="footer">
+		Copyright &copy; 2005-2022, John Swagington.
+	</div>
+</body>
+</html>
