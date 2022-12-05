172beece5784f6f9af630a93ff13d709b5c091be
alalngar <elamite_tepe@proton.me>
2022-12-05 14:06:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <body>
 
 <?php
-//INSERT INTO threads(id, time, author, title, message) VALUES (null, 123, "Me", "Thread Title", "Long Message");
-echo $_SERVER['REMOTE_ADDR'];
+$db = new SQLite3('threads.db');
+$result = $db->query("SELECT * FROM threads");
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    echo $row['title'] . ': ' . $row['message'] . '<br/>';
+}
 ?>
+
+<form action="new_post.php" method="POST">
+    <input type="text" name="author"><br>
+    <input type="text" name="title"><br>
+    <textarea cols="60" rows="5" name="message"></textarea><br>
+    <input type="submit" value="Post Thread">
+</form>
 
 </body>
 </html>
