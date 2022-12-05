a55a6856c907291c0bb3f9a957872c9cfda00793
alalngar <elamite_tepe@proton.me>
2022-12-05 14:25:56 -0600

    Update post.php

--- post.php
+++ post.php
@@ -4,7 +4,9 @@
 
 <?php
 $db = new SQLite3('threads.db');
-$db->query('INSERT INTO threads(id, time, author, title, message) VALUES (null, 123, ' . $_POST['author'] . ', ' . $_POST['title'] . ', ' . $_POST['message'] . ')');
+$query = 'INSERT INTO threads(id, time, author, title, message) VALUES (null, 123, ' . $_POST['author'] . ', ' . $_POST['title'] . ', ' . $_POST['message'] . ')';
+echo $query;
+$db->query($query);
 ?>
 
 </body>
