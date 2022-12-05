94cff33c07e94ced802aee261b3c8bf0b6e9c014
alalngar <elamite_tepe@proton.me>
2022-12-05 12:42:00 -0600

    y

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-    phpinfo();
+phpinfo();
+
 ?>
+
+<form action="new_post.php" method="POST">
+    <input type="text" name="author"><br>
+    <input type="text" name="title"><br>
+    <textarea cols="60" rows="5" name="message"></textarea><br>
+    <input type="submit" value="Post Thread">
+</form>
+
+</body>
+</html>
