5d886921eea584392fa383a487a823920e170e75
alalngar <elamite_tepe@proton.me>
2023-01-26 19:25:16 -0600

    another change

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<a href="index.php"><h1>swagtopia</h1></a>
 		<p>we stand in solidarity with the bajookanese refugees.</p>
 	</div>
-	<div class="posts">
+	<div class="main">
 		<h1>posts</h1>
 		<ul>
 			<li><time>Jan 26 2023</time> - <a href="/posts/post1.html">Test Post</a></li>
