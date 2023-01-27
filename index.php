a93cd03d41b15d413f0317faa897db9c6cd8d704
alalngar <elamite_tepe@proton.me>
2023-01-26 19:18:21 -0600

    new look

--- index.php
+++ index.php
@@ -6,29 +6,14 @@
 </head>
 <body>
 	<div class="title">
-		<h1>swagtopia</h1>
-		<p>the only correct opinion is my own.</p>
+		<a href="index.php"><h1>swagtopia</h1></a>
+		<p>we stand in solidarity with the bajookanese refugees.</p>
 	</div>
-
-	<div class="bar">
-		<a href="index.php">home</a>
-		<a href="forum.php">forum</a>
-	</div>
-
-	<div class="main">
-		<!--
-        <form action="post.php" method="post">
-            <h2>create a thread</h2>
-            <input type="text" name="author" value="Anonymous">
-            <input type="text" name="title" placeholder="Title" minlength="3" maxlength="24" required><br><br>
-            <textarea cols="50" rows="8" placeholder="Lorem ipsum dolor sit amet..." name="message" minlength="32" required></textarea><br>
-            <input type="submit" value="Post Thread">
-        </form>
-		!-->
-	</div>
-
-	<div class="footer">
-		<p>Copyright &copy; 2005-2022, John Swagington.</p>
+	<div class="posts">
+		<h1>posts</h1>
+		<ul>
+			<li><time>Jan 26 2023</time> - <a href="/posts/post1">Test Post</a></li>
+		</ul>
 	</div>
 </body>
 </html>
