2a5baff297b9fe15a5d371eacad044c7dc93cda4
alalngar <elamite_tepe@proton.me>
2023-01-27 21:24:18 -0600

    mottos

--- motto.php
+++ motto.php
@@ -1,10 +1,10 @@
 <?php
 $mottos = [
-    "Swag Inc. stands in solidarity with the Bajookanese refugees.",
+    "Swagtopia stands in solidarity with the Bajookanese refugees.",
+    "Swagtopia denounces the actions of Bajonkistan against Bajookieland.",
+    "Do not talk about Neeemy Nommy CrinkleBoop.",
     "Swagaryans are the most oppressed race.",
-    "I'm Joe Biden, and I approve this message.",
-    "I'm Barack Obama, and I approve this message.",
-    "I'm Donald Trump, and I approve this message."
+    "I'm Joe Biden, and I approve this message."
 ];
 
 echo $mottos[rand(0, sizeof($mottos) - 1)];
