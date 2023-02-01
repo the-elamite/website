f92a7357e6acf37ff439d4deaea25a6106a8a0da
alalngar <elamite_tepe@proton.me>
2023-01-31 20:03:55 -0600

    Update index.php

--- index.php
+++ index.php
@@ -22,5 +22,17 @@
 			<li><a href="/games/dungeon-delver.html">dungeon delver</a></li>
 		</ul>
 	</div>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1514723555021446"
+	 crossorigin="anonymous"></script>
+<!-- ad-1 -->
+<ins class="adsbygoogle"
+	 style="display:block"
+	 data-ad-client="ca-pub-1514723555021446"
+	 data-ad-slot="2863584443"
+	 data-ad-format="auto"
+	 data-full-width-responsive="true"></ins>
+<script>
+	 (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 </body>
 </html>
